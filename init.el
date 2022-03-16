b8288411e912d0e33fb56ed5da86f8a73d862b99
AdrienWehrle <adrien.wehrle@hotmail.fr>
2022-03-16 21:12:29 +0100

    redefine org-bullet fontsizes

--- init.el
+++ init.el
@@ -30,25 +30,9 @@
 
 ;; change default org-level fontsizes
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t 
-		 (:inherit outline-1 
-			   :height 2.0)))) 
- '(org-level-2 ((t 
-		 (:inherit outline-2 
-			   :height 1.5)))) 
- '(org-level-3 ((t 
-		 (:inherit outline-3 
-			   :height 1.2)))) 
- '(org-level-4 ((t 
-		 (:inherit outline-4 
-			   :height 1.0)))) 
- '(org-level-5 ((t 
-		 (:inherit outline-5 
-			   :height 1.0)))))
+  '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+  )
 
 (setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "PROCESSING" "WAITING" "CANCELED" "DONE")))
 
